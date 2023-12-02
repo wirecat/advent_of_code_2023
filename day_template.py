1430192e2b9f771cabd71469dd346d39dcd66f2b
Daniel Johnson <wirecat@github.com>
2023-12-01 19:33:23 -0800

    Day 1 puzzle completed

---
+++ day_template.py
@@ -0,0 +1,18 @@
+import typing
+
+def part_1(data: str) -> int:
+    return 1337
+
+def part_2(data: str) -> int:
+    return 1337
+
+def solve_helper():
+    part_1(test_input)
+    part_2(test_input)
+
+test_input = """"""
+
+input = """"""
+
+if __name__ == "__main__":
+    solve_helper()
